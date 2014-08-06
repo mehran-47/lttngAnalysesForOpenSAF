348b8eea8633972d3601d28af6096d092cf3b0e9
mehran <mehran@in-school.net>
2014-08-06 07:22:01 -0400

    small fix in sessionStarter

--- sessionStarter.sh
+++ sessionStarter.sh
@@ -4,8 +4,8 @@ IFS=' ' read -a SESS_OUT <<< $(lttng create --live)
 lttng enable-channel -k chann1 -C $1M -W $2 && \
 lttng enable-event -k lttng_statedump_start,lttng_statedump_end,lttng_statedump_process_state\
 ,lttng_statedump_file_descriptor,lttng_statedump_vm_map,lttng_statedump_network_interface,lttng_stat\
-edump_interrupt,sched_process_free,sched_switch,sched_process_fork -c chann1 && \
-lttng add-context -k -t pid -t procname -t tid -t ppid -t perf:cache-misses -t perf:major-faults -t perf:branch-load-misses && \
+edump_interrupt,sched_process_free,sched_switch,sched_process_fork -c chann1 ; \
+lttng add-context -k -t pid -t procname -t tid -t ppid -t perf:cache-misses -t perf:major-faults -t perf:branch-load-misses ; \
 lttng start
 lttng list
 
