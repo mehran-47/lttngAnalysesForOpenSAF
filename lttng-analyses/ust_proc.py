7144d92e04aa5f60f2260e705e215a316989af82
root <root@node2.(none)>
2014-08-27 09:55:40 -0400

    temp commit for demo

--- lttng-analyses/ust_proc.py
+++ lttng-analyses/ust_proc.py
@@ -1,45 +1,82 @@
 #!/usr/bin/env python3
 import sys
+import os
+import time
 from babeltrace import *
+from LTTngAnalyzes.common import *
 
-# Check for path arg:
-if len(sys.argv) < 2:
-	raise TypeError("Usage: python example-api-test.py path/to/file")
+class ust_trace():
+	def __init__(self, path, **kwargs):
+		self.path = path
+		self.traces = TraceCollection()
+		self.trace_handle = self.traces.add_trace(self.path, "ctf")
+		self.latest_timestamp = -1
+		self.check_break = False
+		if self.trace_handle is None:
+			raise IOError("Error adding trace")
 
-# Create TraceCollection and add trace:
-traces = TraceCollection()
-trace_handle = traces.add_trace(sys.argv[1], "ctf")
-if trace_handle is None:
-	raise IOError("Error adding trace")
+	def show_event_types(self):
+		print("--- Event list ---")
+		for event_declaration in self.trace_handle.events:
+			print("event : {}".format(event_declaration.name))
+			#if event_declaration.name == "lttng_ust_tracef":
+			for field_declaration in event_declaration.fields:
+				print(field_declaration)
+		print("--- Done ---")
 
-# Listing events
-print("--- Event list ---")
-for event_declaration in trace_handle.events:
-	print("event : {}".format(event_declaration.name))
-	#if event_declaration.name == "lttng_ust_tracef":
-	for field_declaration in event_declaration.fields:
-		print(field_declaration)
-print("--- Done ---")
+	def show_events(self, *types):
+		for event in self.traces.events:
+			if len(types) == 0:
+				for event_type in event:
+					print("event timestamp: "+ ns_to_asctime(event.timestamp) + " : " + str(event.get(event_type)))
+			else:
+				for event_type in types:
+					if event_type in event:
+						print("event timestamp: "+ ns_to_asctime(event.timestamp)  + " : " + str(event.get(event_type)))
+
+	def get_component_pids(self):
+		pass
+
+	def check_new_events(self):
+		oldEventsDict = {}
+		newEventsDict = {}
+		for event in self.traces.events:
+			oldEventsDict[event.timestamp] = event.get("msg")
+		'''
+		if self.latest_timestamp in oldEventsDict:
+			return {}
+		'''
+		self.traces = TraceCollection()
+		self.trace_handle = self.traces.add_trace(self.path, "ctf")
+		for event in self.traces.events:
+			if event.timestamp > self.latest_timestamp:
+				self.latest_timestamp = event.timestamp
+			if event.timestamp not in oldEventsDict:
+				newEventsDict[event.timestamp] = event.get("msg")
+		return newEventsDict
+
+	def keepChecking(self):
+		while not self.check_break:
+			newEvents = self.check_new_events()
+			if len(newEvents) == 0:
+				time.sleep(5)
+			else:
+				print(newEvents)
+				break
 
 
-for event in traces.events:
-	#print("TS: {}, {} : {}".format(event.timestamp,	event.cycles, event.name))
-	print("msg:" + event["msg"])
-	print("timestamp begin: " + str(event.timestamp))
-	#print("timestamp begin: " + str(event.host))
-	#print("timestamp end: " + str(event["timestamp_end"]))
+if __name__ == "__main__":
+	# Check for path arg:
+	if len(sys.argv) < 2:
+		raise TypeError("Usage: python ust-proc.py path/to/file")
 
-'''
-	if event.name == "sched_switch":
-		prev_comm = event["prev_comm"]
-		if prev_comm is None:
-			print("ERROR: Missing prev_comm context info")
-		else:
-			print("sched_switch prev_comm: {}".format(prev_comm))
-
-	if event.name == "exit_syscall":
-		ret_code = event["ret"]
-		if ret_code is None:
-			print("ERROR: Unable to extract ret")
-		else:
-			print("exit_syscall ret: {}".format(ret_code))
+	# Create TraceCollection and add trace:
+	path = sys.argv[1] + "/ust/uid/0/64-bit"
+	while not os.path.isdir(path):
+		print("No UST event yet, waiting")
+		time.sleep(10)
+	
+	ustTrace = ust_trace(path)
+	ustTrace.show_events("msg")
+	#ustTrace.keepChecking()
+	print(ustTrace.check_new_events())
