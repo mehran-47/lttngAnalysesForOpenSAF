aa1b564d595141731d209c36a82275b1e6490b94
Mehran Khan <mehran@in-school.net>
2014-10-29 06:34:53 -0400

    attempted fix for component availability failure

--- lttng-analyses/cputop_i.py
+++ lttng-analyses/cputop_i.py
@@ -117,7 +117,7 @@ class CPUTop():
                     else:
                         self.activeComps[component]['cpu_usage'] = ps.Process(pid).cpu_percent(interval=0.5)
                 else:
-                    self.activeComps[component]['cpu_usage'] = 0.0
+                    del self.activeComps[component]
 
         to_send['component_info'] = self.activeComps
 
