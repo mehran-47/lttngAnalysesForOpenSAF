680a0f5c7c158674d9a616873618a5becb3b05e3
Mehran Khan <mehran@in-school.net>
2014-11-04 22:05:31 -0500

    updated ust_proc.py runner with a proper runner

--- lttng-analyses/ust_proc.py
+++ lttng-analyses/ust_proc.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-import sys
-import os
-import time
-import re
-import json
+import sys, os, time, re, json
 from babeltrace import *
 from LTTngAnalyzes.common import *
-from cputop_i import cputop_init
 
 class ust_trace():
 	def __init__(self, path, **kwargs):
@@ -64,26 +59,57 @@ class ust_trace():
 		self.traces = TraceCollection()
 		self.trace_handle = self.traces.add_trace(self.path, "ctf")
 		for event in self.traces.events:
-			if event.timestamp > self.latest_timestamp:
-				self.latest_timestamp = event.timestamp
 			if event.timestamp not in oldEventsDict:
 				newEventsDict[event.timestamp] = event.get("msg")
 		return newEventsDict
 
+
 if __name__ == "__main__":
-	if len(sys.argv) == 2:
+	if len(sys.argv) == 2 and sys.argv[1]!='--p':
 		path = str(sys.argv[1])
 		ut = ust_trace(path)
 		print('[')
 		for event in sorted(ut.events_as_dict()):
 			print('{\''+str(ns_to_asctime(event))+'\':'+str(ut.events_as_dict()[event])+'},')
 		print(']')
-	elif len(sys.argv) == 3 and sys.argv[2]=='--p':
-		path = str(sys.argv[1])
-		ut = ust_trace(path)
-		allcomps = {}
-		newEvents = {}
-		oldEventsDict = {}
+	elif len(sys.argv) == 2 and sys.argv[1]=='--p':
+		#Persistent UST tracing
+		from utilities.bash import bash
+		from daemon_controller_client import *
+		bashc_v = bash()
+		CREATE_UST = "lttng create --live 2000000 -U net://localhost\nlttng enable-event -u -a --loglevel=TRACE_DEBUG\nlttng start"
+		#Starting UST session
+		try: 
+			(ust_name, ust_path) = start_tracing_with(CREATE_UST, 'ust')
+			while not os.path.exists(ust_path):
+				try:
+					print('UST path does not exist yet')
+					time.sleep(5)
+				except KeyboardInterrupt:
+					destroy_all_sessions()
+					print('Trace-debugging Manually interrupted, quitting.')
+					sys.exit()
+			#UST Session created successfully
+			ut = ust_trace(ust_path)
+			newEventsDict = ut.events_as_dict()
+			allcomps = ut.get_comp_csi(newEventsDict,{})
+			oldEventsDict = {}
+			while True:
+				print(allcomps)
+				print('\n\n\n')
+				oldEventsDict = newEventsDict
+				time.sleep(2)
+				newEventsDict = ut.check_new_events(oldEventsDict)
+				if len(newEventsDict.keys())>0:
+					allcomps = ut.get_comp_csi(newEventsDict,allcomps)
+
+		except TypeError:
+			print('Failed to create UST session. Quitting.')
+			destroy_all_sessions()
+			raise
+		except:
+			raise
+		'''
 		while True:
 			newstr = re.sub("'",'"', str(input("\n\npaste new event string\n>"))) 
 			newEvents = json.loads(newstr)
@@ -92,3 +118,7 @@ if __name__ == "__main__":
 				print(allcomps)
 				for event in newEvents:
 					oldEventsDict[event]=newEvents[event]
+		'''
+	else:
+		raise Exception('usage: "./ust_proc.py --p" for persistent components checking\n\
+			"./ust_proc.py </path/to/trace>"" for checking/viewing all events"')
