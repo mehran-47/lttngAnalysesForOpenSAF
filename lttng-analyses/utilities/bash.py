e85d8a39f6ff7b42f489283182375ab8ddea59c5
Mehran Khan <mehran@in-school.net>
2014-12-02 16:20:35 -0500

    created colored-printing feature

--- lttng-analyses/utilities/bash.py
+++ lttng-analyses/utilities/bash.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
+from subprocess import Popen, PIPE
 
-import subprocess
+def colorPrint(string, *stringAttrs, **allStringAttrs):
+	colorMap = {\
+		'PURPLE' : '\033[95m',\
+		'CYAN' : '\033[96m',\
+		'DARKCYAN' : '\033[36m',\
+		'BLUE' : '\033[94m',\
+		'GREEN' : '\033[92m',\
+		'YELLOW' : '\033[93m',\
+		'RED' : '\033[91m',\
+		'BOLD' : '\033[1m',\
+		'UNDERLINE' : '\033[4m',\
+		'END' : '\033[0m'}
+	if len(stringAttrs)>0:
+		for attr in stringAttrs:
+			string = colorMap.get(str(attr).upper()) + str(string) + colorMap['END']
+		print(string)
+	if len(allStringAttrs)>0:
+		for stringAttrTuple in allStringAttrs.values():
+			for attr in stringAttrTuple:
+				string = colorMap.get(str(attr).upper()) + str(string) + colorMap['END']
+			print(string)	
 
 class bash():
 	def __init__(self, **kwargs):
@@ -16,7 +37,7 @@ class bash():
 		for command in commands:
 			count += 1
 			try:
-				execution = subprocess.Popen(command.split(' '), stdout=subprocess.PIPE)
+				execution = Popen(command.split(' '), stdout=PIPE)
 				output = execution.communicate()
 				if output[0]!=None:
 					self.outputs.append(output[0].decode('utf8'))
@@ -28,3 +49,12 @@ class bash():
 			except:
 				print('Unknown exception')
 				raise
+
+if __name__ == '__main__':
+	while True:
+		try:
+			inpt=[oneArg.strip() for oneArg in input("test 'colorPrint'. Input your string \n> ").strip().split('|')]
+			colorPrint(inpt[0], allStringAttrs=inpt[1:])
+		except KeyboardInterrupt:
+			print('\nBye')
+			break
