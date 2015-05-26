2a081a92967d98eb438e3e5b5cf7e8121dfef478
mehran <mehran@in-school.net>
2015-05-26 15:30:23 -0400

    EEactionListener temporary fix to execute EE

--- lttngAnalyses/EEanalysis/EEactionListener.py
+++ lttngAnalyses/EEanalysis/EEactionListener.py
@@ -11,9 +11,8 @@ def printSent(child_pipe):
 def listen_and_act(child_pipe):
     while True:
         latestInstruction = dict(child_pipe.recv())
-        print(['/home/node1/Downloads/lttngAnalysesForOpenSAF/EE_tool_by_neha/Neha_EETool/ElasticityEngineCMD', latestInstruction.get('SI'), latestInstruction.get('action')])
-        #call(['EE.loc', latestInstruction['SI'], latestInstruction['action']])
-
+        #print(['/home/node1/Downloads/lttngAnalysesForOpenSAF/EE_tool_by_neha/Neha_EETool/ElasticityEngineCMD', latestInstruction.get('SI'), latestInstruction.get('action')])
+        call(['/opt/bin/ElasticityEngineCMD', latestInstruction.get('SI'), latestInstruction.get('action')])
 
 if __name__ == '__main__':
     server = connection(ni.ifaddresses('eth0')[2][0]['addr'], int(sys.argv[1]))
