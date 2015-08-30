cc368fcd1ad08590ef590216adbdf6b216c5ad00
mehran <mehran@in-school.net>
2015-08-30 19:10:10 -0400

    configuration changes work! (somewhat)

--- experiments/pyEESnippets/pyEEsnippets.py
+++ experiments/pyEESnippets/pyEEsnippets.py
@@ -1,47 +1,65 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import immombin, sys
-from immombin import AisException
+import pyImm.immombin, pyImm.immom, pyImm.immomexamples, sys
+from pyImm.immombin import AisException
 
-_flagdict = {
-    'MULTI_VALUE':0x00000001,
-    'RDN':0x00000002,
-    'CONFIG':0x00000100,
-    'WRITABLE':0x00000200,
-    'INITIALIZED':0x00000400,
-    'RUNTIME':0x00010000,
-    'PERSISTENT':0x00020000,
-    'CACHED':0x00040000,
-    }
 
-def _flag_list(flags):
-    l = []
-    for k in _flagdict.keys():
-        if flags & _flagdict[k]:
-            l.append(k)
-    return l
+class osafMod():
+    def __init__(self, *admin):
+        pyImm.immombin.saImmOmFinalize()
+        pyImm.immombin.saImmOmInitialize()        
+        if admin[0:]:
+            self.admin = admin[0]
+            pyImm.immombin.saImmOmAdminOwnerInitialize(self.admin)
+        else:
+            pyImm.immombin.saImmOmAdminOwnerInitialize('monitoring_admin')
 
-def _flag_parse(l):
-    f = 0
-    for n in l:
-        f |= _flagdict[n]
-    return f
+    def modifyObject(self, DN, attrList):
+        try:
+            pyImm.immombin.saImmOmAdminOwnerClear('SA_IMM_SUBTREE', [DN])
+            pyImm.immombin.saImmOmAdminOwnerSet('SA_IMM_SUBTREE', [DN])            
+        except:
+            print('ownership didn\'t work')
+            raise        
+        #'''
+        try:
+            pyImm.immombin.saImmOmCcbInitialize(0)
+            print('attribute list: \n\t' + str(attrList))
+            pyImm.immombin.saImmOmCcbObjectModify(DN, attrList)
+            pyImm.immombin.saImmOmCcbApply()
+            pyImm.immombin.saImmOmCcbFinalize()
+            pyImm.immombin.saImmOmAdminOwnerFinalize()
+        except:
+            print('modification within object didn\'t work')
+            raise
+        #'''
 
-def _get_rdn_attr(class_name):
-    (c,a) = getclass(class_name)
-    for (n,t,f,l) in a:
-        if 'RDN' in f:
-            return (n,t)
-
-if __name__ == '__main__':
-	if sys.argv[1:]:
-		DN = sys.argv[1]
-		immombin.saImmOmInitialize()
-		attributes = {}
-		for(n,t,v) in immombin.saImmOmAccessorGet(DN):
-			attributes[n] = v
-		print(attributes)
-	else:
-		print('DN not provided')
+if __name__ == '__main__':    
+    if sys.argv[3:]:
+        modObj = osafMod('mk')
+        DN = sys.argv[1]
+        attribute = sys.argv[2]
+        value = sys.argv[3]
+        print('chaning the attribute %r to %r in object DN %r' %(attribute, value, DN))
+        print('prev-state:')
+        print(pyImm.immom.getobject(DN))
+        print('owner of the DN')
+        print(pyImm.immomexamples.getadminowner(DN))
+        attrList = [
+            (attribute, 'SAUINT32T', [int(value)])
+        ]        
+        modObj.modifyObject(DN, attrList)
+        print('owner of the DN')
+        print(pyImm.immomexamples.getadminowner(DN))
+    else:
+        print('DN not provided')
+    
+    """
+    pyImm.immombin.saImmOmInitialize()
+    DN = 'opensafImm=opensafImm,safApp=safImmService'
+    for (n,t,v) in pyImm.immombin.saImmOmAccessorGet(DN):
+        if n == 'opensafImmClassNames':
+            print(v)
+    """
 
